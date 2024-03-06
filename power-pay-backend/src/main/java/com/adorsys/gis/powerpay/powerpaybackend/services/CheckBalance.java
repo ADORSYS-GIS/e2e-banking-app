cb15be56db929a6445a46a90871b274feb57042e
Motouom_Wandjeu_Victoire_Josue <motouomvictor@gmail.com>
2024-03-06 11:29:41 +0100

    feat(backend): removed the all the  methods that was found in all the files found in the backend service packege.

--- power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/services/CheckBalance.java
+++ power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/services/CheckBalance.java
@@ -3,5 +3,4 @@ package com.adorsys.gis.powerpay.powerpaybackend.services;
 // creating basic interface class for check balance
 public interface CheckBalance {
 
-    void checkBalance();
-}  
+}
--- power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/services/SendMoney.java
+++ power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/services/SendMoney.java
@@ -4,5 +4,4 @@ package com.adorsys.gis.powerpay.powerpaybackend.services;
 // creating basic interface class for send money
 public interface SendMoney {
 
-    void sendMoney();
 }
