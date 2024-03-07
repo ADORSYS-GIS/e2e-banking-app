32078056c7bd43e0228891ce5f16319e6f074134
Enow Scott <enowscott53@gmail.com>
2024-03-07 16:49:01 +0100

    Implemented a basic Rocket server with Serde serialization and updated the Cargo.toml with some dependencies

--- power-pay-translator/src/main.rs
+++ power-pay-translator/src/main.rs
@@ -1,29 +1,12 @@
-#[allow(dead_code)]
-#![feature(proc_macro_hygiene, decl_macro)]
-
-#[macro_use] extern crate rocket;
-
-use serde::{Serialize, Deserialize};
-use rocket::{get, post, routes};
-use rocket::serde::{Serialize, Deserialize};
-use rocket::serde::json::Json;
-
-#[derive(Serialize, Deserialize)]
-struct MyData {
-    name: String,
-    age: u32,
-}
+#[macro_use]
+extern crate rocket;
+ 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, Rocket server!"
-}
-
-#[post("/data", format = "json", data = "<my_data>")]
-fn process_data(my_data: Json<MyData>) -> String {
-    format!("Received data: Name={}, Age={}", my_data.name, my_data.age)
-}
-
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, process_data])
+fn index() -> &'static str { 
+    "Welcome to the Power Pay App!"
+} 
+ 
+#[launch] 
+fn rocket() -> _ { 
+    rocket::build().mount("/", routes![index]) 
 }
