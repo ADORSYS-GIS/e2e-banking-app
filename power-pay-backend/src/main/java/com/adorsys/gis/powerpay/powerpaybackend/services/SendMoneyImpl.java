8b15d9f3146983c8a1c73ad5460dcf65a5ce2970
forkimenjeckayang <forkimenjeckayang@gmail.com>
2024-03-07 02:12:34 -0500

    Modified(SendMoneyImpl.java):Removed the @Component annotation

--- power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/services/SendMoneyImpl.java
+++ power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/services/SendMoneyImpl.java
@@ -1,10 +1,8 @@
 package com.adorsys.gis.powerpay.powerpaybackend.services;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Component
 @Service
 public class SendMoneyImpl implements SendMoney{
-    
+
 }
