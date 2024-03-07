151c470966cf3cedad68e6dea25b475a2312091e
Ogenbertrand <ogenbertrand@gmail.com>
2024-03-07 11:40:41 +0100

    generate a tailwind.config.js file in the power-pay-frontend/

---
+++ power-pay-frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./power-pay-frontend/index.html',
+  './src/**/*.{html,js}',
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
