6971300d7b0fc6459a29ee7196dd136abf4636d1
Ogenbertrand <ogenbertrand@gmail.com>
2024-03-08 08:42:47 +0100

    create a new config file (/power-pay-frontend/tailwind.config.ts)

---
+++ power-pay-frontend/tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [ './power-pay-frontend/**/*.{html,js}',
+  './src/**/*.{ts,tsx,js}'
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
