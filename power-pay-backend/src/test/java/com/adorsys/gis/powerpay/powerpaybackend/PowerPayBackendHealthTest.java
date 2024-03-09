d88642264fa0eec116e7aaa35beeb4bca16a0ed6
Awambeng <114798938+Awambeng@users.noreply.github.com>
2024-03-09 21:54:53 +0100

    Update PowerPayBackendHealthTest.java
    Fix failing test

--- power-pay-backend/src/test/java/com/adorsys/gis/powerpay/powerpaybackend/PowerPayBackendHealthTest.java
+++ power-pay-backend/src/test/java/com/adorsys/gis/powerpay/powerpaybackend/PowerPayBackendHealthTest.java
@@ -2,13 +2,15 @@ package com.adorsys.gis.powerpay.powerpaybackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 @ContextConfiguration(classes = { PowerPayBackendApplication.class })
 public class PowerPayBackendHealthTest {
 
@@ -19,6 +21,6 @@ public class PowerPayBackendHealthTest {
     public void healthEndpointTest() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/actuator/health"))
             .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("Up"));
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("UP"));
     }
 }
