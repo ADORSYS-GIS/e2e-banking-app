e1d7d335d69327decc4ddc189a5c238ccfa0c2d1
AssahBismarkabah <assah9799@gmail.com>
2024-03-13 12:15:26 +0100

    fix: added fakeimpl

--- power-pay-frontend/src/hooks/StorageContext.tsx
+++ power-pay-frontend/src/hooks/StorageContext.tsx
@@ -8,8 +8,38 @@ interface StorageService<T> {
   clear: () => Promise<boolean>;
 }
 
+// Create the StorageContextData interface.
+interface StorageContextData<T> {
+  getItem: (key: string) => Promise<unknown | undefined>;
+  setItem: (key: string, value: T) => Promise<boolean>;
+  removeItem: (key: string) => Promise<boolean>;
+  clear: () => Promise<boolean>;
+}
+
+
+// Creating  a fake implementation of the StorageContextData interface.
+const fakeStorage: StorageContextData<unknown> = {
+  getItem: async (key: string): Promise< unknown | undefined> => {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item): undefined;
+  },
+  setItem: async (key: string, value: unknown): Promise<boolean> => {
+    localStorage.setItem(key, JSON.stringify(value));
+    return true;
+  },
+  removeItem: async (key: string): Promise<boolean> => {
+    localStorage.removeItem(key);
+    return true;
+  },
+  clear: async (): Promise<boolean> => {
+    localStorage.clear();
+    return true;
+  },
+};
+
+
 // Create the StorageContext with default functions for getItem and setItem.
-const StorageContext = createContext<StorageService<unknown> | undefined>(undefined);
+const StorageContext = createContext<StorageService<unknown> | undefined>(fakeStorage);
 
 // StorageProvider: A React component that wraps the application and provides the storage context with getItem and setItem methods.
 export function StorageProvider<T extends Record<string, T> | undefined>({ initialValue = {} as T, children }: PropsWithChildren<{ initialValue?: T }>) {
@@ -30,37 +60,9 @@ export function StorageProvider<T extends Record<string, T> | undefined>({ initi
     }
   }, [storedValue]);
 
-  // Define the getItem method that retrieves the value from the local storage or returns undefined if it doesn't exist.
-  const getItem = async (key: string): Promise<T | undefined> => {
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : undefined;
-  }
-
-  // Define the setItem method that updates the local storage and the state with the new value.
-  const setItem = async (key: string, value: T): Promise<boolean> => {
-    localStorage.setItem(key, JSON.stringify(value));
-    setStoredValue({ ...storedValue, [key]: value });
-    return true;
-  };
-
-  const removeItem = async (key: string): Promise<boolean> => {
-    localStorage.removeItem(key);
-    setStoredValue((prevData) => {
-      const newData = { ...prevData };
-      delete newData[key];
-      return newData;
-    });
-    return true;
-  };
-
-  const clear = async (): Promise<boolean> => {
-    localStorage.clear();
-    setStoredValue({});
-    return true;
-  };
 
   // Destructure getItem and setItem before using them in the StorageContext.Provider value prop.
-  const contextValue: StorageService<T> = { getItem, setItem, removeItem, clear };
+  const contextValue: StorageService<T> = { getItem: fakeStorage.getItem, setItem: fakeStorage.setItem, removeItem: fakeStorage.removeItem, clear: fakeStorage.clear };
 
   return (
     <StorageContext.Provider value={contextValue as StorageService<unknown>}>
