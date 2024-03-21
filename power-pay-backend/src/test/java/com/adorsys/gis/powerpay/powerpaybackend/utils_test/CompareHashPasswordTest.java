feba7a41fa5c6f13795cf1a2c1946a9767566f0a
chendi <lumblessingc@gmail.com>
2024-03-21 14:46:16 +0100

    added: A test to compare the hash and the raw pin

---
+++ power-pay-backend/src/test/java/com/adorsys/gis/powerpay/powerpaybackend/utils_test/CompareHashPasswordTest.java
@@ -0,0 +1,21 @@
+package com.adorsys.gis.powerpay.powerpaybackend.utils_test;
+
+import org.junit.Test;
+
+import com.adorsys.gis.powerpay.powerpaybackend.utils.DataSecurityService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class CompareHashPasswordTest {
+
+    @Autowired
+    private DataSecurityService dataSecurityService;
+
+    @Test
+    void compareHashPassword(){
+        assertTrue(dataSecurityService.compareHashPassword("mypin", "675243180"));
+    }
+}
