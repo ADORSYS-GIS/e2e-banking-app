ea10e24f616ff427838245c7e7933dad658074b8
forkimenjeckayang <forkimenjeckayang@gmail.com>
2024-03-21 09:16:14 -0400

    util directory(backend):modified DataSEcurityService to include method to check hashed pin with incoming pin

--- power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/utils/DataSecurityService.java
+++ power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/utils/DataSecurityService.java
@@ -3,11 +3,24 @@ package com.adorsys.gis.powerpay.powerpaybackend.utils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import com.adorsys.gis.powerpay.powerpaybackend.domain.User;
+import com.adorsys.gis.powerpay.powerpaybackend.repository.UserRepository;
+
 @Component
 public class DataSecurityService {
 
+    private static final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    private UserRepository userRepository;
+
     public String hashData(String data) {
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         return passwordEncoder.encode(data);
+    }
+
+    public boolean compareHashPassword(String pin, String phoneNumber) {
+        User user = userRepository.findById(phoneNumber).get();
+        String hashPin = user.getPin();
+        boolean result = passwordEncoder.matches(pin, hashPin);
+        return result;
     }
 }
