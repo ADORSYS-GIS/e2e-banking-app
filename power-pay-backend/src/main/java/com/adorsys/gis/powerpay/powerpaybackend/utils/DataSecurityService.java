609aff9c967b5301d64f3b3f010e26bf96294b00
forkimenjeckayang <forkimenjeckayang@gmail.com>
2024-03-24 13:55:54 -0400

    Modified DataSecurityService(backend):Updated the method to compare a specific data with it's hashed value

--- power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/utils/DataSecurityService.java
+++ power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/utils/DataSecurityService.java
@@ -3,24 +3,20 @@ package com.adorsys.gis.powerpay.powerpaybackend.utils;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import com.adorsys.gis.powerpay.powerpaybackend.domain.User;
-import com.adorsys.gis.powerpay.powerpaybackend.repository.UserRepository;
-
 @Component
 public class DataSecurityService {
 
     private static final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    private UserRepository userRepository;
-
+    //Method to hash Data
     public String hashData(String data) {
         return passwordEncoder.encode(data);
     }
 
-    public boolean compareHashPassword(String pin, String phoneNumber) {
-        User user = userRepository.findById(phoneNumber).get();
-        String hashPin = user.getPin();
-        boolean result = passwordEncoder.matches(pin, hashPin);
+    //Method to compare a given String with hashed value
+    public boolean compareHashedData(String data, String hashedData) {
+        hashedData = hashData(data);
+        boolean result = passwordEncoder.matches(data, hashedData);
         return result;
     }
 }
