be4ef8d92f40d26c24993325c9fe8569ace0c3e6
forkimenjeckayang <forkimenjeckayang@gmail.com>
2024-03-25 12:19:15 -0400

    Handling Exception(backend):Implemented the UserRegistration exception class to handle errors at  runtime

---
+++ power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/utils/UserRegistrationException.java
@@ -0,0 +1,7 @@
+package com.adorsys.gis.powerpay.powerpaybackend.utils;
+
+public class UserRegistrationException extends RuntimeException {
+    public UserRegistrationException(String message) {
+        super(message);
+    } 
+}
