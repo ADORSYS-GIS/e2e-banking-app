efc920429a8911de9f42dff01d475af84bc73234
Awambeng <awambengrodrick@gmail.com>
2024-03-26 13:07:49 +0100

    (backend): updated user registration exception for better handling

--- power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/utils/UserRegistrationException.java
+++ power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/utils/UserRegistrationException.java
@@ -1,7 +1,7 @@
 package com.adorsys.gis.powerpay.powerpaybackend.utils;
 
 public class UserRegistrationException extends RuntimeException {
-    public UserRegistrationException(String message) {
-        super(message);
-    } 
+    public UserRegistrationException(String message, Exception exception) {
+        super(message, exception);
+    }
 }
