785d934c6ac0535f403cb2348d8b87fef5a5d0c3
forkimenjeckayang <forkimenjeckayang@gmail.com>
2024-03-26 19:43:27 -0400

    UserRegistrationService(backend): modified create procedure method

--- power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/services/UserRegistrationService.java
+++ power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/services/UserRegistrationService.java
@@ -1,8 +1,10 @@
 package com.adorsys.gis.powerpay.powerpaybackend.services;
 
+import com.adorsys.gis.powerpay.powerpaybackend.domain.User;
+
 public interface UserRegistrationService {
 
-    public int registerUser(String phoneNumber, String userName, String pin);
+    public User registerUser(String phoneNumber, String userName, String pin);
 
     public int createProcedure(String userName);
 
