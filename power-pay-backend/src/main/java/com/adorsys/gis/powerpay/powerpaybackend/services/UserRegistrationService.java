d4958eb7fe704487411aee3a9612b3849d899888
Awambeng <awambengrodrick@gmail.com>
2024-03-26 13:12:03 +0100

    (backend): updated UserRegistration to UserRegistrationService

--- power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/services/UserRegistrationService.java
+++ power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/services/UserRegistrationService.java
@@ -1,7 +1,7 @@
 package com.adorsys.gis.powerpay.powerpaybackend.services;
 
 public interface UserRegistrationService {
-    
+
     public int registerUser(String phoneNumber, String userName, String pin);
 
     public int createProcedure(String userName);
