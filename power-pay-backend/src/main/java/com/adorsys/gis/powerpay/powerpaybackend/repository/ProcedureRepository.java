eebf0d2d3826643185102da84daf54aa93285f7f
forkimenjeckayang <forkimenjeckayang@gmail.com>
2024-03-26 19:48:33 -0400

    ProcedureRepository(backend):removed file and replaced with UserRegistrationRepository

--- power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/repository/ProcedureRepository.java
+++
@@ -1,8 +0,0 @@
-package com.adorsys.gis.powerpay.powerpaybackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.adorsys.gis.powerpay.powerpaybackend.domain.Procedure;
-
-public interface ProcedureRepository extends JpaRepository<Procedure, Integer> {
-    
-}
