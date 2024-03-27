649fa94e4c9f01833372d2b1c8aee04221d0afee
Motouom_Wandjeu_Victoire_Josue <motouomvictor@gmail.com>
2024-03-27 10:17:44 +0100

    Modify: Switched from css to Tailwind CSS for styling the ok page

--- power-pay-frontend/src/component/okpage.tsx
+++ power-pay-frontend/src/component/okpage.tsx
@@ -42,14 +42,17 @@ const OKpage: React.FC = () => {
 //building the ok page for successful transfer
 const OKPage = () => {
   return (
-    <div className="card">
-      <div className="ok-page">
-        <h1>Success!</h1>
-        <br></br>
-        <p>The money was successfully transfered.</p>
-        <br></br>
-      </div> 
-      <button className ="okbtn">OK</button>
+    <div className="flex justify-center items-center p-10 mb-34 border border-white rounded-3xl bg--500 text-white">
+      <div className="card">
+        <div className="ok-page">
+          <h1>Success!</h1>
+          <br></br>
+          <p>The money was successfully transfered.</p>
+        </div> 
+        <div className="mt-16">
+        <button className="w-96 mt-16 bg-Gray10-color rounded-3xl p-2 text-center text-white bg-green-500">OK</button>
+        </div>
+      </div>
     </div>
   );
 };
