dce6e58e65607e7cdc6c74213e83b241a5b81fff
Enow Scott <enowscott53@gmail.com>
2024-04-02 14:19:42 +0100

    Implemented the index.tsx for handeling the input manually and scan qr pages

---
+++ power-pay-frontend/src/Components/index.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import QRScannerComponent from './scan_qr';
+
+const PaymentOptions: React.FC = () => {
+  const [showQRScanner, setShowQRScanner] = useState<boolean>(false);
+  const [showInputManually, setShowInputManually] = useState<boolean>(false);
+
+  const handleScanQRClick = () => {
+    setShowQRScanner(true);
+  };
+
+  const handleInputManuallyClick = () => {
+    setShowInputManually(true);
+  };
+
+  return (
+    <div className="container">
+      <h2>Payment Options</h2>
+      <p>Please select how you would like to send money:</p>
+      <button onClick={handleInputManuallyClick}>Input Manually</button>
+      <button onClick={handleScanQRClick}>Scan QR Code</button>
+
+      {showQRScanner && <QRScannerComponent />}
+      {showInputManually && <InputManuallyComponent />}
+    </div>
+  );
+};
+
+export default PaymentOptions;
+
--- power-pay-frontend/src/main.tsx
+++ power-pay-frontend/src/main.tsx
@@ -1,13 +1,14 @@
 
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import QRScannerComponent from './Components/scan_qr.tsx'
+import PaymentOptions from './Components/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <QRScannerComponent/>
+    <PaymentOptions/>
   </React.StrictMode>,
 )
