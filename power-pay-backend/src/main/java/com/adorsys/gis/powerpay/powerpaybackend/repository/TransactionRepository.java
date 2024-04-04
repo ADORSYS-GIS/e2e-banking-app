e6ac9320cc6b56b99f7774d6079044243334704a
nkwajudetambe <nkwajudetambe@gmail.com>
2024-04-04 14:20:02 +0100

    Added JPQL query for calculateBalaceByPhoneNumber in TransactionRepository

--- power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/repository/TransactionRepository.java
+++ power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/repository/TransactionRepository.java
@@ -3,8 +3,13 @@ package com.adorsys.gis.powerpay.powerpaybackend.repository;
 import com.adorsys.gis.powerpay.powerpaybackend.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface TransactionRepository  extends JpaRepository<Transaction, Long> {
+
+    @Query("SELECT SUM(CASE WHEN t.receiverPhoneNumber = :phoneNumber THEN t.amount ELSE -t.amount END) " +
+            "FROM Transaction t WHERE t.receiverPhoneNumber = :phoneNumber OR t.phoneNumber = :phoneNumber")
+
     Double calculateBalaceByPhoneNumber(String phoneNumber);
 }
