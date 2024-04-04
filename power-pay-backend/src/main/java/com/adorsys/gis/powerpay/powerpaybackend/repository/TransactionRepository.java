a79e448b72f785ea2f91cef1bfdaaa620091ca24
nkwajudetambe <nkwajudetambe@gmail.com>
2024-04-04 09:56:47 +0100

    Add TransactionRepository for managing Transaction entities

--- power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/repository/TransactionRepository.java
+++ power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/repository/TransactionRepository.java
@@ -1,4 +1,9 @@
 package com.adorsys.gis.powerpay.powerpaybackend.repository;
 
-public interface TransactionRepository {
+import com.adorsys.gis.powerpay.powerpaybackend.domain.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository  extends JpaRepository<Transaction, Long> {
 }
