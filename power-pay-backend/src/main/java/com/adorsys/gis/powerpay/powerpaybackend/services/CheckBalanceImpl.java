3f2662b577fd08352f0fac64c7914bde38af823a
nkwajudetambe <nkwajudetambe@gmail.com>
2024-04-04 13:33:53 +0100

    Modified CheckBalanceImpl to use TransactionRepository for balance calculation

--- power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/services/CheckBalanceImpl.java
+++ power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/services/CheckBalanceImpl.java
@@ -1,30 +1,24 @@
 package com.adorsys.gis.powerpay.powerpaybackend.services;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.Query;
+import com.adorsys.gis.powerpay.powerpaybackend.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service //  annotation to ensure classes are recognized as Spring Beans
 public class CheckBalanceImpl implements CheckBalance  {
-    private final EntityManager entityManager;
+    private final TransactionRepository transactionRepository;
 
     @Autowired
-    public CheckBalanceImpl(EntityManager entityManager){
+    public CheckBalanceImpl(TransactionRepository transactionRepository){
 
-        this.entityManager = entityManager;
+        this.transactionRepository = transactionRepository;
     }
 
     @Override
-    public Double checkBalance(String phoneNumber) throws UsernameNotFoundException {
-        String queryString = "SELECT SUM(CASE WHEN t.receiverPhoneNumber = :phoneNumber THEN t.amount ELSE -t.amount END) " +
-                "FROM Transaction t WHERE t.receiverPhoneNumber = :phoneNumber OR t.phoneNumber = :phoneNumber";
-
-        Query query = entityManager.createQuery(queryString);
-        query.setParameter("phoneNumber", phoneNumber);
-        Double balance = (Double) query.getSingleResult();
-        return balance != null ? balance : 0.0;
+    public Double checkBalance(String phoneNumber) throws UsernameNotFoundException{
+        Double balance = transactionRepository.calculateBalaceByPhoneNumber(phoneNumber);
+        return  balance != null ? balance : 0.0;
     }
 }
 
