057f5b1f7496507de042c4d18a495a43d2ccef0e
nkwajudetambe <nkwajudetambe@gmail.com>
2024-04-04 13:32:42 +0100

    Updated TransactionRepository to include calculateBalanceByPhoneNumber method.

--- power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/repository/TransactionRepository.java
+++ power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/repository/TransactionRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepository  extends JpaRepository<Transaction, Long> {
+    Double calculateBalaceByPhoneNumber(String phoneNumber);
 }
