3b8f808d95dbf27b90d7900d4aaa0a496f5c0c46
kouamschekina <kouamdelphine82@gmail.com>
2024-04-09 16:40:53 +0100

    fix: fixed the SendMoneyImpl test and CompareHashPassword test

--- power-pay-backend/src/test/java/com/adorsys/gis/powerpay/powerpaybackend/utils_test/CompareHashPasswordTest.java
+++ power-pay-backend/src/test/java/com/adorsys/gis/powerpay/powerpaybackend/utils_test/CompareHashPasswordTest.java
@@ -1,12 +1,15 @@
 package com.adorsys.gis.powerpay.powerpaybackend.utils_test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.adorsys.gis.powerpay.powerpaybackend.utils.DataSecurityService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes={DataSecurityService.class})
