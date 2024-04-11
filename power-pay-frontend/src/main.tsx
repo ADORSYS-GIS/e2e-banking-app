852257743a5699a1c9566a98b127f5dce40bf679
Enow Scott <enowscott53@gmail.com>
2024-04-11 13:53:12 +0100

    feat: Implemented the sqr code

--- power-pay-frontend/src/main.tsx
+++ power-pay-frontend/src/main.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import QRScannerComponent from './components/scan_rq.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <QRScannerComponent/>
   </React.StrictMode>,
 )
