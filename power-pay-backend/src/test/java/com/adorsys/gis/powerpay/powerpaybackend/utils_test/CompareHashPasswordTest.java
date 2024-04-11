8f24cc7b4f34a0713ddffb865a2d94cb4b522942
kouamschekina <146363175+kouamschekina@users.noreply.github.com>
2024-04-11 17:09:58 +0100

    fix: backend tests that caused the CI/CD pipeline to fail (#129)
    * fix: fixed the SendMoneyImpl test and CompareHashPassword test
    
    * fix: fixed the SendMoneyImpl test and CompareHashPassword test
    
    * test: commented defective tests
    
    * test: uncommented defective tests
    
    * fix(test): fixed the BackendHealthTest and ApplicationsTest

--- power-pay-backend/src/test/java/com/adorsys/gis/powerpay/powerpaybackend/utils_test/CompareHashPasswordTest.java
+++ power-pay-backend/src/test/java/com/adorsys/gis/powerpay/powerpaybackend/utils_test/CompareHashPasswordTest.java
@@ -1,14 +1,18 @@
 package com.adorsys.gis.powerpay.powerpaybackend.utils_test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.adorsys.gis.powerpay.powerpaybackend.utils.DataSecurityService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes={DataSecurityService.class})
 public class CompareHashPasswordTest {
 
     @Autowired
