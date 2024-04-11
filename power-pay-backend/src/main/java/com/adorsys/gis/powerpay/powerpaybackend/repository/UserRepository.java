31651613d81884be886d30b8f205c9e71e745ff4
kouamschekina <kouamdelphine82@gmail.com>
2024-04-11 15:43:32 +0100

    fix(test): fixed the BackendHealthTest and ApplicationsTest

--- power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/repository/UserRepository.java
+++ power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.adorsys.gis.powerpay.powerpaybackend.repository;
 
-import com.adorsys.gis.powerpay.powerpaybackend.domain.Transaction;
 import com.adorsys.gis.powerpay.powerpaybackend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository  extends JpaRepository<Transaction, Long> {
-    Optional<User> findByUserId(String userId);
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByPhoneNumber(String phoneNumber);
 }
