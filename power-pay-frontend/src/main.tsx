365874623179da75ab101a650bf64d0a707e6182
Motouom Victoire <motouomvictor@gmail.com>
2024-04-12 06:56:30 +0100

    Update(frontend): updated the PinInput page and added more funtionalities.

--- power-pay-frontend/src/main.tsx
+++ power-pay-frontend/src/main.tsx
@@ -1,35 +1,15 @@
 
 import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
-import { Outlet } from 'react-router-dom'
 import App from './App.tsx'
-import OKPage from './components/okpage.tsx'
 import './index.css'
-import PinInput from './components/pinInput.tsx'
+// import PinInput from './components/pinInput.tsx'
+import OKPage from './components/okpage.tsx'
 
-
-
-const router = createBrowserRouter([
-  {
-      path: "/",
-      element: <PinInput/>,
-      
-  },
-
-  {
-    path: "/OkPage",
-    element: <OKPage />,
-  },
-  
-
- 
-
-])
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-<RouterProvider router={router} />
-<Outlet />
-<App />
+    <App />
+    {/* <PinInput /> */}
+    <OKPage />
   </React.StrictMode>,
 )
