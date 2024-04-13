12ffd5b63c9f7cc221b6400c772fef4748c36491
Ngha-Boris <borisngha6@gmail.com>
2024-04-13 07:47:15 +0100

    Fix: fixed the test which was failing due to the changes made in the  main

--- power-pay-frontend/src/main.tsx
+++ power-pay-frontend/src/main.tsx
@@ -1,34 +1,11 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import App from './App.tsx'
-import PaymentOptions from './components/index.tsx'
-import './App.css';
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import { Outlet } from "react-router-dom";
-import UserInfo from './components/userinfo.tsx';
-const router = createBrowserRouter([
-  {
-      path: "/",
-      element: <PaymentOptions/>,
-  },
-
-  {
-    path: "/userinfo",
-    element: <UserInfo />,
-  },
- 
-
-])
+import App from './App.tsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-<RouterProvider router={router} />
-<Outlet />
-{/* <App /> */}
+    <App />
   </React.StrictMode>,
 )
