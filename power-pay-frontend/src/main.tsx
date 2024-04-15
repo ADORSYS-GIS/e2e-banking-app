d84faeb796756a3d26405e17490cd6e9a374f926
Motouom Victoire <motouomvictor@gmail.com>
2024-04-15 11:30:05 +0100

    Update(front-end): Used routing for better management of pages

--- power-pay-frontend/src/main.tsx
+++ power-pay-frontend/src/main.tsx
@@ -1,15 +1,11 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-// import PinInput from './components/pinInput.tsx'
-import OKPage from './components/okpage.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-    {/* <PinInput /> */}
-    <OKPage />
+      <App />
+      {/* adding the path to the okpage  */}
   </React.StrictMode>,
 )
