818bb3d5b11e227b1dec1bd8020d0fcdc6a85198
nkwajudetambe <nkwajudetambe@gmail.com>
2024-06-11 12:27:59 +0100

    feat(backend): Update API endpoint to use phone number for balance checking

--- power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/repository/UserRepository.java
+++ power-pay-backend/src/main/java/com/adorsys/gis/powerpay/powerpaybackend/repository/UserRepository.java
@@ -9,5 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository  extends JpaRepository<Transaction, Long> {
-    Optional<User> findByUserId(String userId);
+    //Optional<User> findByUserId(String userId);
+
+    Optional<User> findByPhoneNumber(String phoneNumber);
 }
