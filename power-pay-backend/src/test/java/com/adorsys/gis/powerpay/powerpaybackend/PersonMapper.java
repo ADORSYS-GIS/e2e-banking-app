6c9da1d3b15790567b42f6847a5899f45c181f8b
kouamschekina <kouamdelphine82@gmail.com>
2024-06-19 09:49:14 +0100

    Revert "Revert "fix(backend): modified PersonMapper file""
    This reverts commit 250338f832eb51a0ea4c72690bb52c76f6453eab.

--- power-pay-backend/src/test/java/com/adorsys/gis/powerpay/powerpaybackend/PersonMapper.java
+++ power-pay-backend/src/test/java/com/adorsys/gis/powerpay/powerpaybackend/PersonMapper.java
@@ -5,9 +5,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-import com.adorsys.gis.powerpay.powerpaybackend.domain.model.Person;
-import com.adorsys.gis.powerpay.powerpaybackend.services.dto.PersonDto;
-
 @Mapper
 public interface PersonMapper {
     PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
