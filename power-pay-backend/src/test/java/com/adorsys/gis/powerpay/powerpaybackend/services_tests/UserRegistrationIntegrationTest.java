b74d9f2274c6ae6de10c68d55f0e1736f037d811
mbunwe-victor <mbunwevicki@gmail.com>
2024-06-22 08:13:03 +0100

    Update: updated the UserRegistrationIntegrationTest file

--- power-pay-backend/src/test/java/com/adorsys/gis/powerpay/powerpaybackend/services_tests/UserRegistrationIntegrationTest.java
+++ power-pay-backend/src/test/java/com/adorsys/gis/powerpay/powerpaybackend/services_tests/UserRegistrationIntegrationTest.java
@@ -8,6 +8,7 @@ import com.adorsys.gis.powerpay.powerpaybackend.services.userRegistrationControl
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
